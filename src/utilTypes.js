e342737af4891542d9a0a3a10513bd0f97568986
Jacob Parker <jacob.parker@autotrip.co.uk>
2016-09-09 10:49:46 +0100

    Flow

--- src/utilTypes.js
+++ src/utilTypes.js
@@ -1,9 +1,9 @@
 // @flow
 export type Curry2<A, B, C> =
-  ((a: A, b: B) => C) |
+  ((a: A, b: B) => C) &
   ((a: A) => (b: B) => C);
 
 export type Curry3<A, B, C, D> =
-  ((a: A, b: B, c: C) => D) |
-  ((a: A, b: B) => (c: C) => D) |
+  ((a: A, b: B, c: C) => D) &
+  ((a: A, b: B) => (c: C) => D) &
   ((a: A) => (b: B) => (c: C) => D);
